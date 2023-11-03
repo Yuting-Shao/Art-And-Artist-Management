59b729c930edd7fb40006dbed9f89f44f219757c
Yuting Shao <105260656+Yuting-Shao@users.noreply.github.com>
2023-11-03 19:30:26 -0400

    minor revise

--- query.sql
+++ query.sql
@@ -1,3 +1,6 @@
+/*
+1. Join of at least 3 tables: Retrieve all artworks displayed in the exhibition `Modern Perspectives`
+*/
 SELECT 
     a.title AS artworkTitle,
     ar.name AS artistName,
@@ -13,6 +16,10 @@ JOIN
 WHERE 
     e.ExhibitionName = 'Modern Perspectives';
 
+
+/*
+2. Subquery: Find artworks that are priced above the average price of all artworks
+*/
 SELECT 
     title,
     price 
@@ -21,6 +28,10 @@ FROM
 WHERE 
     price > (SELECT AVG(price) FROM Artwork);
 
+
+/*
+3. GROUP BY with HAVING clause: Find the artists who have more than 2 artworks in the database
+*/
 SELECT 
     artistID,
     COUNT(artworkID) AS NumberOfArtworks 
@@ -31,6 +42,10 @@ GROUP BY
 HAVING 
     NumberOfArtworks > 2;
 
+
+/*
+4. Complex search criterion: Retrieve artworks created after 1/1/2020 that are either paintings priced above $50 or sculptures priced below $20
+*/
 SELECT 
     title,
     medium,
@@ -42,6 +57,10 @@ WHERE
     creationDate > '2020-01-01' AND 
     ((medium = 'painting' AND price > 50) OR (medium = 'sculpture' AND Price < 20));
 
+
+/*
+5. Advanced query mechanisms using `SELECT CASE/WHEN`: Classify artworks based on their price range
+*/
 SELECT 
     title,
     price,
