e3d7edf45d0d62cb3caf8c7789cbb79628777176
Yuting Shao <105260656+Yuting-Shao@users.noreply.github.com>
2023-11-10 13:30:09 -0500

    implementing app

--- app/db/dbConnector_Sqlite.js
+++ app/db/dbConnector_Sqlite.js
@@ -20,6 +20,68 @@ async function getArtists() {
     return artistdata;
 }
 
+async function createArtist(data) {
+    const db = await connect();
+    const { name, biography, style, totalExhibitions } = data;
+    await db.run(`INSERT INTO Artist (name, biography, style, totalExhibitions) VALUES (?, ?, ?, ?)`, [name, biography, style, totalExhibitions]);
+    return { name, biography, style, totalExhibitions };
+}
+
+async function updateArtist(data) {
+    const db = await connect();
+    const { artistID, name, biography, style, totalExhibitions } = data;
+    await db.run(`UPDATE Artist SET name = ?, biography = ?, style = ?, totalExhibitions = ? WHERE artistID = ?`, [name, biography, style, totalExhibitions, artistID]);
+    return { artistID, name, biography, style, totalExhibitions };
+}
+
+async function deleteArtist(data) {
+    const db = await connect();
+    const { artistID } = data;
+    await db.run(`DELETE FROM Artist WHERE artistID = ?`, [artistID]);
+    return { artistID };
+}
+
+async function getArtworksByArtist(artistID) {
+    const db = await connect();
+    const artworkdata = await db.all(`SELECT artworkID, title, price, artistID
+    FROM Artwork
+    WHERE artistID = ?
+    ORDER BY artworkID DESC
+    LIMIT 20;`, [artistID]);
+
+    console.log("db connector got data", artworkdata.length);
+
+    return artworkdata;
+}
+
+async function createArtwork(data) {
+    const db = await connect();
+    const { title, medium, dimensions, creationDate, price, availabilityStatus, artistID } = data;
+    await db.run(`INSERT INTO Artwork (title, medium, dimensions, creationDate, price, availabilityStatus, artistID) VALUES (?, ?, ?, ?, ?, ?, ?)`, [title, medium, dimensions, creationDate, price, availabilityStatus, artistID]);
+    return { title, medium, dimensions, creationDate, price, availabilityStatus, artistID };
+}
+
+async function updateArtwork(data) {
+    const db = await connect();
+    const { artworkID, title, medium, dimensions, creationDate, price, availabilityStatus, artistID } = data;
+    await db.run(`UPDATE Artwork SET title = ?, medium = ?, dimensions = ?, creationDate = ?, price = ?, availabilityStatus = ?, artistID = ? WHERE artworkID = ?`, [title, medium, dimensions, creationDate, price, availabilityStatus, artistID, artworkID]);
+    return { artworkID, title, medium, dimensions, creationDate, price, availabilityStatus, artistID };
+}
+
+async function deleteArtwork(data) {
+    const db = await connect();
+    const { artworkID } = data;
+    await db.run(`DELETE FROM Artwork WHERE artworkID = ?`, [artworkID]);
+    return { artworkID };
+}
+
 module.exports = {
-    getArtists
+    getArtists,
+    createArtist,
+    updateArtist,
+    deleteArtist,
+    getArtworksByArtist,
+    createArtwork,
+    updateArtwork,
+    deleteArtwork
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,89 @@
 let express = require('express');
 let router = express.Router();
 
-const { getArtists } = require('../db/dbConnector_Sqlite.js');
+const { getArtists, createArtist, updateArtist, deleteArtist, getArtworksByArtist, createArtwork, updateArtwork, deleteArtwork } = require('../db/dbConnector_Sqlite.js');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const artists = await getArtists();
   console.log("route / called - got artists", artists.length);
   res.render('index', { title: 'Art and Artist Management System', artists });
+});
+
+/* GET artworks for a specific artist. */
+router.get('/artist/:artistID', async function(req, res, next) {
+  const artistID = parseInt(req.params.artistID);
+  const artworks = await getArtworksByArtist(artistID);
+  console.log("route /artist/:artistID called - got artworks", artworks.length);
+  res.render('artworks', { artistID, artworks });
+});
+
+/* Render a form to create a new artist. */
+router.get('/artist/new', async function(req, res, next) {
+  res.render('createArtist', { title: 'Create Artist' });
+});
+
+/* Handle form submission to create a new artist. */
+router.post('/artist', async function(req, res, next) {
+  const artist = await createArtist(req.body);
+  console.log("route /artist/new called - created artist", artist);
+  res.redirect('/');
+});
+
+/* Render a form to update an existing artist. */
+router.get('/artist/:artistID/edit', async function(req, res, next) {
+  const artistID = parseInt(req.params.artistID);
+  const artist = await getArtists(artistID);
+  console.log("route /artist/:artistID/edit called - got artist", artist);
+  res.render('updateArtist', { artist });
+});
+
+/* Handle form submission to update an existing artist. */
+router.put('/artist/:artistID', async function(req, res, next) {
+  const artist = await updateArtist(req.body);
+  console.log("route /artist/:artistID/edit called - updated artist", artist);
+  res.redirect('/');
+});
+
+/* Handle deletion of an artist. */
+router.delete('/artist/:artistID', async function(req, res, next) {
+  const artist = await deleteArtist(req.body);
+  console.log("route /artist/:artistID called - deleted artist", artist);
+  res.redirect('/');
+});
+
+/* Render a form to create a new artwork. */
+router.get('/artwork/new', async function(req, res, next) {
+  res.render('createArtwork', { title: 'Create Artwork' });
+});
+
+/* Handle form submission to create a new artwork. */
+router.post('/artwork', async function(req, res, next) {
+  const artwork = await createArtwork(req.body);
+  console.log("route /artwork/new called - created artwork", artwork);
+  res.redirect('/');
+});
+
+/* Render a form to update an existing artwork. */
+router.get('/artwork/:artworkID/edit', async function(req, res, next) {
+  const artworkID = parseInt(req.params.artworkID);
+  const artwork = await getArtworks(artworkID);
+  console.log("route /artwork/:artworkID/edit called - got artwork", artwork);
+  res.render('updateArtwork', { artwork });
+});
+
+/* Handle form submission to update an existing artwork. */
+router.put('/artwork/:artworkID', async function(req, res, next) {
+  const artwork = await updateArtwork(req.body);
+  console.log("route /artwork/:artworkID/edit called - updated artwork", artwork);
+  res.redirect('/');
+});
+
+/* Handle deletion of an artwork. */
+router.delete('/artwork/:artworkID', async function(req, res, next) {
+  const artwork = await deleteArtwork(req.body);
+  console.log("route /artwork/:artworkID called - deleted artwork", artwork);
+  res.redirect('/');
 });
 
 module.exports = router;
